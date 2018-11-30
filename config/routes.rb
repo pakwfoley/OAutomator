4ea73693f9678a11a61073f4a2efd4a8d3f02cb6
Patrick Foley <patrick.foley@procore.com>
2018-11-30 15:31:10 -0800

    update dumb controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'tests_controller#new'
-  get 'tests_controller/new'
+  root to: 'tests#new'
+  get 'tests/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
